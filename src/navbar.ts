20e6cb58239ee2560de1a5924fe003673750f147
kevinvantus <kevinvantus@gmail.com>
2023-05-19 13:43:34 +0100

    change link color on hover

--- src/navbar.ts
+++ src/navbar.ts
@@ -2,17 +2,34 @@ const navbar = `
   <nav>
     <ul>
       <li><a href="#">home</a></li>
-      <li><a href="#about" class=${
-        location.hash === "#about" ? "active" : ""
-      }>about</a></li>
-      <li><a href="#skills"  class=${
-        location.hash === "#skills" ? "active" : ""
-      }>skills</a></li>
+      <li><a href="#about">about</a></li>
+      <li><a href="#skills">skills</a></li>
       <li><a href="#projects"  class=${
         location.hash === "#projects" ? "active" : ""
       }>projects</a></li>
     </ul>
   </nav>
 `;
+
+window.addEventListener("load", (_) => {
+  const links = [...document.querySelectorAll("nav a")] as HTMLAnchorElement[];
+
+  const activeLink = links.find((link) => link.hash === location.hash);
+  activeLink?.classList.add("active");
+});
+
+window.addEventListener("hashchange", (_) => {
+  const links = [...document.querySelectorAll("nav a")] as HTMLAnchorElement[];
+
+  [...links].forEach((link) => {
+    const isMatch = link.hash === location.hash;
+
+    if (!link.classList.contains("active") && isMatch) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
 
 export default navbar;
