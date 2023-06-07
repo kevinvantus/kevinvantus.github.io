b6ca11339dbf9787af3187b0def15f3b25730d66
kevinvantus <kevinvantus@gmail.com>
2023-06-07 14:18:16 +0100

    about section

--- src/about.ts
+++ src/about.ts
@@ -3,34 +3,51 @@ const about = `
     <h2>About</h2>
 
     <article>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. At dolore,
-      numquam quod aperiam quis cumque placeat? Odit ex, consectetur
-      exercitationem hic error fugiat incidunt excepturi, accusamus alias,
-      suscipit iste provident nisi distinctio. Ducimus similique sint minus
-      omnis!
+      <p>Hi, I'm a passionate Frontend Developer (on my way to becoming a 
+        Full stack developer😁) with a strong
+      focus on building beautiful user interfaces and user experiences. With 
+      over 2 years of experience, I have honed my skills in HTML, CSS, and JavaScript, 
+      allowing me to bring designs to life and craft seamless interactions.
+      </p>
+
+      <p>
+      I thrive on staying up-to-date with the latest frontend technologies
+      and frameworks like React and Angular, enabling me to 
+      build scalable and responsive web applications. I have a keen eye
+    for detail, and I'm dedicated to optimizing performance, accessibility, 
+    and cross-browser compatibility.
+      </p>
+        
+      <p>
+      Collaborating closely with designers 
+      and backend developers, I strive to create intuitive interfaces 
+      that make a lasting impression. I am driven by the desire to create
+       elegant, functional, and user-centric web experiences. 
+       Let's work together to turn your vision into reality.
+      </p>
     </article>
 
-    <article>
-      <h3>Experience</h3>
-      <p class="period">March 2021 - September 2021</p>
-      <h4 class="company">Aitechma</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, quasi?
-        Molestias nisi repellat asperiores aliquam dolore molestiae suscipit
-        facere natus.
-      </p>
-    </article>
-    
-    <article>
-      <p class="period">January 2022 - March 2023</p>
-      <h4 class="company">Tyms Africa</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, quasi?
-        Molestias nisi repellat asperiores aliquam dolore molestiae suscipit
-        facere natus.
-      </p>
-    </article>
-  </section>
-`;
+    </section>
+    `;
+// <article>
+//   <h3>Experience</h3>
+//   <p class="period">March 2021 - September 2021</p>
+//   <h4 class="company">Aitechma</h4>
+//   <p>
+//     Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, quasi?
+//     Molestias nisi repellat asperiores aliquam dolore molestiae suscipit
+//     facere natus.
+//   </p>
+// </article>
+
+// <article>
+//   <p class="period">January 2022 - March 2023</p>
+//   <h4 class="company">Tyms Africa</h4>
+//   <p>
+//     Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, quasi?
+//     Molestias nisi repellat asperiores aliquam dolore molestiae suscipit
+//     facere natus.
+//   </p>
+// </article>
 
 export default about;
